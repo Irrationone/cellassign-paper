6a4d74c3aa5196ea98477d2ed5634b1e1ce8846f
Allen Zhang <allenz9410@gmail.com>
2018-10-30 20:40:28 +0000

    Draft of annotation component

--- pipelines/follicular_analysis/R/annotate_cellassign.R
+++ pipelines/follicular_analysis/R/annotate_cellassign.R
@@ -33,13 +33,24 @@ sce <- readRDS(sce_path)
 broad_assignments <- readRDS(args$broad)
 specific_assignments <- readRDS(args$specific)
 
+# Add MAP assignments
 sce$cellassign_cluster_broad <- broad_assignments$cell_type
 sce$cellassign_cluster_specific <- specific_assignments$cell_type
 
+# Add probabilities
+broad_gamma <- as.data.frame(broad_assignments$mle_params$gamma)
+colnames(broad_gamma) <- paste0(colnames(broad_gamma), " (broad)")
+specific_gamma <- as.data.frame(specific_assignments$mle_params$gamma)
+
+sce@colData <- bind_cols(sce@colData %>% as.data.frame, 
+                         broad_gamma,
+                         specific_gamma) %>% DataFrame(check.names = FALSE)
+
+# Set master celltype label
 sce$celltype <- sce$cellassign_cluster_specific
 
 # Save annotated SCE
-saveRDS(sce_annotated, args$outfname)
+saveRDS(sce, args$outfname)
 
 cat("Completed.\n")
 
