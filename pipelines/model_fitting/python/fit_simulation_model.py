25ba51bfae253715bfb08b8d8f9867afb755da94
Allen Zhang <allenz9410@gmail.com>
2018-11-21 22:57:38 +0000

    Initial commit of fitting code

---
+++ pipelines/model_fitting/python/fit_simulation_model.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import pymc3 as pm
+import scipy as sc
+import theano
+import theano.tensor as tt
+import copy
+import feather
+import re
+import argparse
+
+import matplotlib.pyplot as plt
+import statsmodels.stats.stattools
+import statsmodels.graphics.gofplots
+
